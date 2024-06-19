23e7509623f43704e33d90cd78b7c33d72c47bf1
Andres Apostol <aandresaapostol@gmail.com>
2024-06-19 05:41:29 -0400

    Inital layouts and add tailwinds in project

--- gesinvui/src/app/app.routes.ts
+++ gesinvui/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '@dashboard/shared/components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LayoutComponent,
+        children: [
+
+        ]
+    }
+];
